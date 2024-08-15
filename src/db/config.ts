c985cd4d751d3f24aa79a06b17941aff1159fe38
CheeseRabit <383468066@qq.com>
2024-08-15 17:34:20 +1000

    this runs on oscar's machine

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,10 +1,19 @@
 import { Sequelize } from 'sequelize';
 
+const dialect = process.env.DB_DIALECT;
+
+if (!dialect) {
+  throw new Error("DB_DIALECT environment variable is not set.");
+}
+
 const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  { host: process.env.DATABASE_HOST, dialect: process.env.DATABASE_DIALECT as any },
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  { 
+    host: process.env.DB_HOST, 
+    dialect: dialect as any 
+  },
 );
 
-export default sequelize
+export default sequelize;
